5cfa73a07a427cc2c2246a92ffed987a0da53439
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2019-11-17 12:12:09 +0900

    테스트: 데이터 추출 후 list에 올바르게 넣어지는지
    2015.01의 1호선 데이터를 추출해보았음:
    - 지하철역 list
    - 해당 지하철역의 04시-05시 승차인원 list

--- crawling.py
+++ crawling.py
@@ -5,27 +5,49 @@ import requests
 startnum = 1
 endnum = 10
 use_mon = 201501
+subway_name = '1호선'
 SubwayInfo = {}
 
-use_mon_list = []
-line_num = []
-sub_station = []
+sub_sta_nm_list = []
 # 00시승차, 하차인원 (시간단위:1시간)
-FOUR_RIDE_NUM = []
-FOUR_ALIGHT_NUM = []
-FIVE_RIDE_NUM = []
-FIVE_ALIGHT_NUM = []
-SIX_RIDE_NUM = []
-SIX_ALIGHT_NUM = []
+four_ride_num_list = []
+four_alight_num_list = []
+five_ride_num_list = []
+five_alight_num_list = []
+six_ride_num_list = []
+six_alight_num_list = []
 
-if endnum <= 1000:
+if endnum <= 600:
     url = 'http://openapi.seoul.go.kr:8088/647047637a656b6434385274774659/xml/' \
-          'CardSubwayTime/'+str(startnum)+'/'+str(endnum)+'/'+str(use_mon)+'/1호선'
+          'CardSubwayTime/'+str(startnum)+'/'+str(endnum)+'/'+str(use_mon)+'/'+subway_name
     req = requests.get(url)
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
-    test = soup.find_all('sub_sta_nm')
 
-    # print(soup) : 파싱한 html
-    # print(test) : 해당 태그 내용들 모두 찾기
-    # print(test[0].text) : 태그 내용들 중 하나만 찾기
+    sub_sta_nm = soup.find_all('sub_sta_nm')
+
+    four_ride_num = soup.find_all('four_ride_num')
+    four_alight_num = soup.find_all('four_alight_num')
+    five_ride_num = soup.find_all('five_ride_num')
+    five_alight_num = soup.find_all('five_alight_num')
+    six_ride_num = soup.find_all('six_ride_num')
+    six_alight_num = soup.find_all('six_alight_num')
+
+    for code in sub_sta_nm:
+        sub_sta_nm_list.append(code.text)
+    for code in four_ride_num:
+        four_ride_num_list.append(code.text)
+    for code in four_alight_num:
+        four_alight_num_list.append(code.text)
+    for code in five_ride_num:
+        five_ride_num_list.append(code.text)
+    for code in five_alight_num:
+        five_alight_num_list.append(code.text)
+    for code in six_ride_num:
+        six_ride_num_list.append(code.text)
+    for code in six_alight_num:
+        six_alight_num_list.append(code.text)
+
+    print(sub_sta_nm_list)
+    print(four_ride_num_list)
+
