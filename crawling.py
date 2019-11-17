26565c6a83de8ebb0589fc30a23619f5dccb8382
iDaeun <47049072+ekdms2309@users.noreply.github.com>
2019-11-17 11:22:58 +0900

    테스트: url로부터 데이터 뽑아올 수 있는지
    크롤링 데이터: 역별, 시간대별 탑승 및 하차 승객 수
    데이터 출처 - 서울열린데이터광장

--- crawling.py
+++ crawling.py
@@ -1,3 +1,31 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+
+startnum = 1
+endnum = 10
+use_mon = 201501
+SubwayInfo = {}
+
+use_mon_list = []
+line_num = []
+sub_station = []
+# 00시승차, 하차인원 (시간단위:1시간)
+FOUR_RIDE_NUM = []
+FOUR_ALIGHT_NUM = []
+FIVE_RIDE_NUM = []
+FIVE_ALIGHT_NUM = []
+SIX_RIDE_NUM = []
+SIX_ALIGHT_NUM = []
+
+if endnum <= 1000:
+    url = 'http://openapi.seoul.go.kr:8088/647047637a656b6434385274774659/xml/' \
+          'CardSubwayTime/'+str(startnum)+'/'+str(endnum)+'/'+str(use_mon)+'/1호선'
+    req = requests.get(url)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    test = soup.find_all('sub_sta_nm')
+
+    # print(soup) : 파싱한 html
+    # print(test) : 해당 태그 내용들 모두 찾기
+    # print(test[0].text) : 태그 내용들 중 하나만 찾기
